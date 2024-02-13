13fd1c789f9f670be27feb18c4143b5714dfd95c
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-13 09:12:08 +0000

    Refactor get_context function to retrieve customer name from Contact

--- digital_subscriptions/www/download_list/index.py
+++ digital_subscriptions/www/download_list/index.py
@@ -5,11 +5,23 @@ sitemap = 1
 
 
 def get_context(context):
-	user = frappe.get_doc("User", frappe.session.user)
+	user = frappe.session.user
+
+	contact_name = frappe.db.get_value("Contact", {"email_id": user})
+	if contact_name:
+		contact = frappe.get_doc("Contact", contact_name)
+		for link in contact.links:
+			if link.link_doctype == "Customer":
+				customer = frappe.get_doc("Customer", link.link_name)
+
+	if not customer:
+		customer_name = ""
+	else:
+		customer_name = customer.name
 
 	subscriptions = frappe.get_all(
 			"File Subscription",
-			filters={"customer": user.full_name, "disabled": 0, "ends_on": ['>', frappe.utils.now()]},
+			filters={"customer": customer_name, "disabled": 0, "ends_on": ['>', frappe.utils.now()]},
 			fields=["name", "item", "item.item_name", "starts_on", "ends_on"]
 		)
 
