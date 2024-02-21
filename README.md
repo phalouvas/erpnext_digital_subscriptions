0d3e01310a24ade94f9dcc327d864b8efaffef74
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-21 07:44:26 +0000

    Add user and subscription migration scripts

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Sell subscriptions for file download
 MIT
 
 #### How to migrate users from Joomla
-visit url: https://erpnext.kainotomo.com/api/method/digital_subscriptions.digital_subscriptions.hooks.migrate.migrate
+For users: https://erpnext.kainotomo.com/api/method/digital_subscriptions.digital_subscriptions.hooks.migrate.create_users
+For subscriptions: https://erpnext.kainotomo.com/api/method/digital_subscriptions.digital_subscriptions.hooks.migrate.create_subscriptions
--- digital_subscriptions/digital_subscriptions/hooks/migrate.py
+++ digital_subscriptions/digital_subscriptions/hooks/migrate.py
@@ -2,14 +2,15 @@ import frappe
 from frappe.exceptions import PermissionError
 from frappe.core.doctype.user.user import create_contact
 from erpnext.portal.utils import party_exists
+from datetime import datetime, timedelta
 
 @frappe.whitelist(allow_guest=False)
-def migrate():
+def create_users():
 	if not frappe.session.user == "Administrator":
 		raise PermissionError("Only the Administrator can perform the migration.")
 	
 	# Get all users from table `rc11v_users`
-	users = frappe.db.sql("SELECT * FROM `rc11v_users` LIMIT 2", as_dict=True)
+	users = frappe.db.sql("SELECT * FROM `rc11v_users`", as_dict=True)
 	# For each user in `rc11v_users`, create a new user in `tabUser`
 	for user in users:
 		customer = create_customer(user)
@@ -82,3 +83,46 @@ def link_customer(user):
 			fullname = frappe.utils.get_fullname(user)
 			party.customer_name = fullname
 			party.save(ignore_permissions=True)
+
+@frappe.whitelist(allow_guest=False)
+def create_subscriptions():
+	if not frappe.session.user == "Administrator":
+		raise PermissionError("Only the Administrator can perform the migration.")
+	
+	# Get all sales order with status "Completed" and "transaction_date" not older than 1 year
+	one_year_ago = datetime.now() - timedelta(days=365)
+	data = frappe.db.sql(f"""
+		SELECT 
+			TItem.name AS item_name,
+			TOrder.transaction_date AS order_date,
+			TOrder.name AS order_name,
+			TOrder.customer AS customer
+		FROM `tabSales Order Item` AS TOrderItem
+			LEFT JOIN `tabSales Order` AS TOrder ON TOrderItem.parent = TOrder.name
+			LEFT JOIN `tabItem` AS TItem ON TOrderItem.item_code = TItem.item_code
+			LEFT JOIN `tabItem Group` AS TItemGroup ON TItem.item_group = TItemGroup.name
+		WHERE TOrder.transaction_date >= '{one_year_ago}'
+			AND TOrder.status = 'Completed'
+			AND TItemGroup.name = 'Joomla! Extensions Shop'
+		ORDER BY TOrder.transaction_date
+		LIMIT 1;
+	""", as_dict=True)
+
+	for order in data:
+		# Check if the subscription already exists in `tabSubscription`
+		subscription_exists = frappe.get_all("File Subscription", filters={"item": order.item_name, "sales_invoice": order.invoice_name}, fields=["name"])
+		if not subscription_exists:
+			subscription_doc = frappe.get_doc({
+				"doctype": "Subscription",
+				"subscription_name": order.item_name,
+				"customer": order.customer,
+				"start_date": order.order_date,
+				"end_date": order.order_date + timedelta(days=365),
+				"status": "Active",
+				"billing_interval": "Yearly",
+				"billing_period": 1,
+				"billing_cycle": 1
+			}).insert()
+			frappe.db.commit()
+
+	return ["Subscriptions created successfully.", data]
